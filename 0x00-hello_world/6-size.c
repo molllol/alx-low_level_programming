910ff455d983653b633a3c53e71a51816bda5fdc
molllol <pogmog7@gmail.com>
2023-02-17 06:52:07 -0800

    Task 6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char c;
+  int i;
+  long int li;
+  long long int lli;
+  float f;
+
+  /*Size of the variables*/
+  printf("Size of char: %zu bytes\n", sizeof(c));
+  printf("Size of int: %zu bytes\n", sizeof(i));
+  printf("Size of long int: %zu bytes\n", sizeof(li));
+  printf("Size of long long int: %zu bytes\n", sizeof(lli));
+  printf("Size of float: %zu bytes\n", sizeof(f));
+
+  retrun (0);
+}
